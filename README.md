f7e1246cce85351d78462982d3e7e8c759904e2b
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2022-01-26 05:38:35 +0330

    added logout section, made some final changes

--- README.md
+++ README.md
@@ -1,13 +1,5 @@
-.settings/
-bin/
-tmp/
-.metadata
-.classpath
-.project
-*.tmp
-*.bak
-*.swp
-*~.nib
-local.properties
-.loadpath
-.factorypath
+<h1>Login App By Cookie</h1>
+<p>in this project i have used servlets and cookies to store user status</p>
+<p>There are 3 servlets in this project: 1-LoginServlet: This is the servlet which gets 
+user's username and password and checks it against the database. if they are correct a cookie is created. 2-ProfileServlet: in this servlet we check for the cookie. if there is a cookie then we're logged in and if there is no cookie then we are not. 3-LogoutServlet: When the user clicks logout button, this servlet destroys the cookie so that user gets logged out</p>
+<p>Also for connecting to database i've used JDBC and postgresql</p>
--- src/main/java/com/hossein/login/LogoutServlet.java
+++ src/main/java/com/hossein/login/LogoutServlet.java
@@ -2,10 +2,12 @@ package com.hossein.login;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
 @WebServlet("/LogoutServlet")
 public class LogoutServlet extends HttpServlet {
@@ -16,11 +18,24 @@ public class LogoutServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		PrintWriter out = response.getWriter();
+		
+		response.setContentType("text/html");
+		request.getRequestDispatcher("links.html").include(request, response);
+		
+		// Make a New Cookie
+		// Name It Same As Our Login Cookie
+		Cookie cookie = new Cookie("username", "");
+		
+		// Set It To Expire As Soon As It Gets Made
+		cookie.setMaxAge(0);
+		response.addCookie(cookie);
+		
+		out.print("You Have Successfully Logged Out");
+		out.close();
+		
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
-	}
 
 }
