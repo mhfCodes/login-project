c769d251892d635f0e4cf7d9a003bf20e991b4aa
mhfCodes <mohammadhossein.farrokhi.78@gmail.com>
2022-01-26 05:15:17 +0330

    added profile section

--- src/main/java/com/hossein/login/ProfileServlet.java
+++ src/main/java/com/hossein/login/ProfileServlet.java
@@ -2,11 +2,14 @@ package com.hossein.login;
 
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.PrintWriter;
 
+// Define The URL of Servlet
 @WebServlet("/ProfileServlet")
 public class ProfileServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -16,12 +19,38 @@ public class ProfileServlet extends HttpServlet {
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
+		
+		PrintWriter out = response.getWriter();
+		
+		response.setContentType("text/html");
+		request.getRequestDispatcher("links.html").include(request, response);
+		
+		// Get Cookies
+		Cookie[] cookies = request.getCookies();
+		
+		// Check If There Are Any Cookies
+		if (cookies != null) {
+			
+			// If We Have Cookies
+			// Check For a Cookie Named username
+			// We Could've Also Used cookies[0]
+			// But We're Considering We Have Multiple Cookies
+			for (Cookie c : cookies) {
+				if (c.getName().equals("username")) {
+					out.print("<b>Welcome To Your Profile " + c.getValue() + "</b>");
+				}
+			}
+			
+		} else {
+			
+			// If There Are No Cookies
+			// Then Tell User To Login First
+			out.print("Please Login First");
+			request.getRequestDispatcher("login.html").include(request, response);
+		
+		}
 	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		out.close();
 	}
 
 }
